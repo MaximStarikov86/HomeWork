8bcfa68d03d80d58b0316d12d9b990c01793d212
Maxim <starikov.m.86@gmail.com>
2023-03-09 21:15:31 +0500

    task 52

---
+++ Task 52/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом
+// столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого
+// столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadInt(string text) // Ввод данных
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int rows, int cols) // Задание рандомного двумерного массива
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(-10, 11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix) //Вывод массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t"); // "\t" Табуляция, позволяет сделать правильные отступы*\\
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void Average(int[,] array) //Нахождение среднего арифметического каждого столбца 2х мерного массива
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum += array[j, i];
+        }
+        Console.WriteLine($"Среднее арифметическое столбца {i + 1} = {sum / array.GetLength(0)}; ");
+        sum = 0;
+    }
+}
+
+int rows = ReadInt("Введите количество строк: ");
+int cols = ReadInt("Введите количество столбцов: ");
+System.Console.WriteLine();
+var myMatrix = GenerateMatrix(rows, cols);
+PrintMatrix(myMatrix);
+System.Console.WriteLine();
+Average(myMatrix);
